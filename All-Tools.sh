18449916b7cd4aacb8e6c5959b82a6165ff906e0
CYB3RT3CH <60990704+cyb3rt3ch@users.noreply.github.com>
2020-07-21 01:13:52 +0530

    Add files via upload

---
+++ All-Tools.sh
@@ -0,0 +1,65 @@
+clear
+echo ""
+echo ""
+echo "           _    _ _   _____           _       "
+echo "          / \  | | | |_   _|__   ___ | |___   "
+echo "         / _ \ | | |   | |/ _ \ / _ \| / __|  "
+echo "        / ___ \| | |   | | (_) | (_) | \__ \  "
+echo "       /_/   \_\_|_|   |_|\___/ \___/|_|___/  "
+
+
+echo ""
+echo "          The Script Is Made By CYB3RT3CH"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-------------------------------"
+echo "Installtion Process Is Started" 
+echo "-------------------------------"
+echo ""
+echo ""
+
+apt-get update -y                    
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+apt install wcalc -y 
+apt install openssl -y
+apt install bmon -y
+apt-get upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-------------------------------------------"
+echo " All Basic Packages And Tools Is Installed"
+echo "-------------------------------------------"
+echo ""
+echo ""
+exit
---
+++ README.md
@@ -0,0 +1,25 @@
+# All Tools
+
+In this i have build all main Tools in one script only one time rum this script it will automatically install all important Tools
+
+![Screenshot_2020-07-21-00-48-29-869_com termux](https://user-images.githubusercontent.com/60990704/87978459-0b5ea280-caee-11ea-96ff-c69e999a9d5e.jpg)
+
+
+
+## Installing (Kali Linux/Termux):
+
+```
+git clone https://github.com/cyb3rt3ch/All-Tools
+```
+```
+cd All-Tools
+```
+```
+Bash All-Tools.sh
+```
+
+
+It will automatically install all the tools.
+
+## Donate Us:
+   https://bit.ly/DonaTeUs
